2f131bedfcb57f9f553ae7e775c6e8519c1960ea
Cameron Zandkarimi <camcamcz@gmail.com>
2016-11-18 13:35:44 -0500

    added delete button

--- src/js/controllers/detail.js
+++ src/js/controllers/detail.js
@@ -18,15 +18,26 @@ function DetailController ($scope, $http, $stateParams, $state) {
 
   }
 
-    $scope.addLike = function (image) {
-      console.log(image)
-      if (image.like_count === null) {
-        image.like_count = 0;
-      }
-      image.like_count++;
-      let updateImage = $http.patch(SERVER + '/images/' + image.id, image);
-      console.log(updateImage)
+  $scope.addLike = function (image) {
+    console.log(image)
+    if (image.like_count === null) {
+      image.like_count = 0;
     }
+    image.like_count++;
+    let updateImage = $http.patch(SERVER + '/images/' + image.id, image);
+    console.log(updateImage)
+  }
+
+  $scope.delete = function (image) {
+  $http.delete(SERVER + '/images/' + image.id).then(function (resp) {
+      console.log(resp);
+
+    $scope.images = $scope.images.filter(function (x) {
+       return x.id !== image.id
+    });
+  });
+  $state.go('home');
+  };
 }
 
 DetailController.$inject = ['$scope', '$http', '$stateParams', '$state'];
